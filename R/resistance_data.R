5f7cd5479aaca0c0c2bd36c52596407065e0e838
wpeterman <bill.peterman@gmail.com>
2014-06-13 11:43:12 -0500

    added data sets to package

---
+++ R/resistance_data.R
@@ -0,0 +1,9 @@
+#' @docType data
+#' @name resistance_surfaces
+#' @title  Raster stack of resistance surfaces
+#' @format Raster stack object of length 4
+#' @usage resistance_surfaces
+#' @description  There are four raster surfaces contained in the \code{resistance_surfaces} raster stack: two continuous surfaces, one 3-class categorical surface, and one 2-class feature surface
+#' @keywords datasets
+#' 
+NULL
