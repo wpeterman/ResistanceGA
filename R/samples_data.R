8093e3828f77d47b1043dc4c25436ec724d68e53
wpeterman <bill.peterman@gmail.com>
2014-06-13 11:43:48 -0500

    added data

---
+++ R/samples_data.R
@@ -0,0 +1,9 @@
+#' @docType data
+#' @name samples
+#' @title  ID and spatial location of samples
+#' @format A 25 x 3 data frame
+#' @usage samples
+#' @description  Sample file to be used with examples in the vignette
+#' @keywords datasets
+#' 
+NULL
