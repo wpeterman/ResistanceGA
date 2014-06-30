a58b321fa9baf5b4583c33eed37bc8fffa89ef1d
wpeterman <bill.peterman@gmail.com>
2014-06-30 13:07:52 -0500

    updated Run_gdistance

--- R/ResistanceGA_Functions.R
+++ R/ResistanceGA_Functions.R
@@ -1000,14 +1000,13 @@ Run_CS2 <- function(CS.inputs,GA.inputs,r,EXPORT.dir=GA.inputs$Write.dir, File.n
 #' Execute gdistance
 #' 
 #' @param gdist.inputs Object created from running \code{\link[ResistanceGA]{CS.prep}} function
-#' @param GA.inputs Object created from running \code{\link[ResistanceGA]{GA.prep}} function
 #' @param r Accepts two types of inputs. Provide either the path to the resistance surface file (.asc) or specify an R RasterLayer object
 #' @return A costDistance matrix object from gdistance
-#' @usage Run_gdistance(gdist.inputs, GA.inputs, r)
+#' @usage Run_gdistance(gdist.inputs, r)
 
 #' @export
 #' @author Bill Peterman <Bill.Peterman@@gmail.com>
-Run_gdistance <- function(gdist.inputs,GA.inputs,r){
+Run_gdistance <- function(gdist.inputs, r){
   if(class(r)[1]!='RasterLayer') {
     r<-raster(r)   
   }  
